ce17dfc97e42c4e557aacb6e9cfbc9ef02ba4933
Erik Rahm <erik.rahm@cybergrx.com>
2019-06-07 11:15:16 -0400

    Creating react-outside-click-hook repository and npm package.

---
+++ index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import isElement from "lodash.iselement";
+
+type Props = {
+  element: React.MutableRefObject<null | HTMLDivElement>;
+  onOutsideClick: (isInside: boolean) => void;
+  disabled: boolean;
+};
+
+export const useOutsideClick = ({
+  element,
+  onOutsideClick,
+  disabled
+}: Props) => {
+  const onMouseUp = (e: MouseEvent) => {
+    const isInside =
+      element.current && isElement(element.current)
+        ? element.current.contains(e.target as HTMLDocument)
+        : false;
+    if (!isInside) {
+      onOutsideClick(isInside);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mouseup", onMouseUp);
+    return () => {
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+  }, [disabled]);
+};
